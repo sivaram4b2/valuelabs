65b79af408e3f81c5996e718c0e2fa73f9c50eae
sivaram4b2 <prathisrk@gmail.com>
2022-01-03 11:36:30 +0530

    commit

---
+++ ValueLabs/src/main/java/ValueLabs/ValueLabs/App.java
@@ -0,0 +1,64 @@
+package ValueLabs.ValueLabs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String s="Java Programming";
+        
+        //Sivaram
+        
+        char[] charString=s.toCharArray();
+        
+        char[] temp=new char[charString.length];
+        
+        char[] characterstoberemoved= {'a','r','m'};
+        
+        List<Character> list=new ArrayList<Character>();
+        Arrays.asList(characterstoberemoved);
+       
+        int count=0;
+        
+        for(int index=0;index<charString.length;index++)
+        {
+//       	if(!(charString[index]=='a'|| charString[index]=='r'))
+			 if(!list.contains(charString[index])) 
+        	{
+        		temp[count]=charString[index];
+        		
+        		count++;
+        		
+        	}
+        }
+        
+        for(char ele:temp)
+        {
+        	System.out.print(ele);
+        }
+    }
+    
+    
+    //frame 1,id=1[frame2,id=2[frame 3,id=3]
+    
+    
+    //try{
+  
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
